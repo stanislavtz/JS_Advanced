b3fbcbae24e5db48b24b84539e81b56f74473fb7
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-05-11 12:48:26 +0300

    Locked-Profile update, using Event Delegation

--- L06. DOM-Manipulation/07. Locked-Profile/app.js
+++ L06. DOM-Manipulation/07. Locked-Profile/app.js
@@ -1,34 +1,62 @@
+/* 
+    Solution, using Event Delegation.
+*/
+
 function lockedProfile() {
-    const $profiles = document.querySelectorAll('.profile');
-    [...$profiles].forEach(profile => {
-        const lockRadio = profile.querySelectorAll('input[value]')[0];
-        const unlockRadio = profile.querySelectorAll('input[value]')[1];
-
-        unlockRadio.addEventListener('click', () => {
-            lockRadio.removeAttribute('checked')
-            unlockRadio.setAttribute('checked', '');
-        })
-
-        lockRadio.addEventListener('click', () => {
-            unlockRadio.removeAttribute('checked')
-            lockRadio.setAttribute('checked', '');
-        })
-        
-        const $button = profile.getElementsByTagName('button')[0];
-        const hiddenField = profile.querySelector('div[id]');
-
-        $button.addEventListener('click', action);
-        function action(){
-            if (unlockRadio.hasAttribute('checked')) {
-                if ($button.textContent === 'Show more') {
-                    hiddenField.style.display = 'block';
-                    $button.textContent = 'Hide it';
+    const main = document.querySelector('#main');
+    main.addEventListener('click', (e) => {
+        const element = e.target;
+        if (element.localName === 'button') {
+            const lockRt = element.parentElement.querySelector('input[value="lock"]');
+            const profileInfo = element.previousElementSibling;
+            if (!lockRt.checked) {
+                if (!profileInfo.style.display || profileInfo.style.display === "none") {
+                    profileInfo.style.display = "block";
+                    element.textContent = "Hide it";
                 }
-                else{
-                    hiddenField.style.display = 'none';
-                    $button.textContent = 'Show more';
+                else {
+                    profileInfo.style.display = "none";
+                    element.textContent = "Show more";
                 }
             }
         }
     });
 }
+
+/* 
+    Solution using Array collection of profiles
+*/
+// function lockedProfile() {
+//     const profiles = document.querySelectorAll('.profile');
+//     [...profiles].forEach(profile => {
+//         const lockRadio = profile.querySelector('input[value="lock"]');
+//         const unlockRadio = profile.querySelector('input[value="unlock"]');
+
+//         unlockRadio.addEventListener('click', () => {
+//             unlockRadio.setAttribute('checked', '');
+//             lockRadio.removeAttribute('checked');
+//         })
+
+//         lockRadio.addEventListener('click', () => {
+//             lockRadio.setAttribute('checked', '');
+//             unlockRadio.removeAttribute('checked');
+//         })
+
+//         const button = profile.querySelector('button');
+//         const hiddenField = profile.querySelector('div');
+
+//         button.addEventListener('click', showMore);
+//         function showMore(){
+//             if (unlockRadio.hasAttribute('checked')) {
+//                 if (button.textContent === 'Show more') {
+//                     hiddenField.style.display = 'block';
+//                     button.textContent = 'Hide it';
+//                 }
+//                 else{
+//                     hiddenField.style.display = 'none';
+//                     button.textContent = 'Show more';
+//                 }
+//             }
+//         }
+//     });
+// }
