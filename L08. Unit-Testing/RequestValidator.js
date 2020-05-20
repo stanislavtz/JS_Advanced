34f75fe3466c3c4aadbb254a00c538033b35ddd9
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-05-20 09:22:51 +0300

    Uni-testing - RequestValidator update

--- L08. Unit-Testing/RequestValidator.js
+++ L08. Unit-Testing/RequestValidator.js
@@ -1,68 +1,34 @@
-function result(obj = {}){
-    const inputKeys = Object.keys(obj);
+function checkRequest(obj) {
+    const model = ["method", "uri", "version", "message"]
+    const specialChars = ['<', '>', '\\', '&', "'", '"'];
 
-    const properties = ['method', 'uri', 'version', 'message'];
-    const methods = ['GET', 'POST', 'DELETE', 'CONNECT'];
-    const versions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
-    let spChar = ['<', '>', '\\', '&', '\'', '"',];
+    for (const element of model) {
+        if(obj["message"] !== "" && !obj[element]) {
+            const message = element === "uri" ? element.toUpperCase() : element[0].toUpperCase() + element.slice(1);
+            throw new Error(`Invalid request header: Invalid ${message}`);
+        }
+    }
+    
+    const methods = ["GET", "POST", "DELETE", "CONNECT"];
+    if (!methods.includes(obj.method)) {
+        throw new Error("Invalid request header: Invalid Method");
+    }
 
+    const uriRegex = /[A-Za-z.\*]+/g;
+    if (!uriRegex.test(obj.uri) || obj.uri.split(' ').length > 1) {
+        throw new Error("Invalid request header: Invalid URI");
+    }
 
-    properties.forEach(el => {
-        if(!inputKeys.includes(el.toLowerCase())){
-            if (el.toUpperCase() === 'URI') {
-                throw new Error(`Invalid request header: Invalid ${el.toUpperCase()}`);
-            };
-            throw new Error(`Invalid request header: Invalid ${el[0].toUpperCase()}${el.substring(1)}`);
+    const versions = ["HTTP/0.9", "HTTP/1.0", "HTTP/1.1", "HTTP/2.0"];
+    if (!versions.includes(obj.version)) {
+        throw new Error("Invalid request header: Invalid Version");
+    }
+    
+    for (const char of specialChars) {
+        if (obj.message.includes(char)) {
+            throw new Error("Invalid request header: Invalid Message");
         }
-    });
-
-    inputKeys.forEach(el => {
-        if (el === 'method') {
-            if (!methods.includes(obj[el])) {
-                let message = `Invalid request header: Invalid ${el[0].toUpperCase()}${el.substring(1)}`;
-                throw new Error(message);
-            }
-        }
-        else if (el === 'uri') {
-            let re = /[A-Za-z.\*]+/;
-            if (!re.test(obj[el]) || obj[el].split(' ').length > 1) {
-                let message = `Invalid request header: Invalid ${el.toUpperCase()}`;
-                throw new Error(message);
-            }
-        }
-        else if (el === 'version') {
-            if (!versions.includes(obj[el])) {
-                let message = `Invalid request header: Invalid ${el[0].toUpperCase()}${el.substring(1)}`;
-                throw new Error(message);
-            }
-        }
-        else if (el === 'message') {
-            spChar.forEach(char => {
-                if (obj[el].includes(char)) {
-                    let message = `Invalid request header: Invalid ${el[0].toUpperCase()}${el.substring(1)}`;
-                    throw new Error(message);
-                }
-            });
-        }
-    });
+    }
 
     return obj;
 }
-
-let obj = {
-    method: 'GET',
-    uri: 'kkk jjjj',
-    version: 'HTTP/0.8',
-    message: ''
-};
-
-//console.log(result(obj));
-// expect(() => result(obj)).to.throw(Error).which.has.property('message', 'Invalid request header: Invalid URI');
-// obj.uri = '';
-// console.log(result(obj));
-
-// expect(() => result(obj)).to.throw(Error).which.has.property('message', 'Invalid request header: Invalid URI');
-obj.uri = 'gib ';
-console.log(result(obj));
-
-// expect(() => result(obj)).to.throw(Error).which.has.property('message', 'Invalid request header: Invalid URI');
