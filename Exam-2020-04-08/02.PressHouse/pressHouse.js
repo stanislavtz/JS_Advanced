56c84108f3e0dc454c8e0b2f632f256ae568fd7a
Tzekov <stanislavtz@gmail.com>
2020-06-24 15:51:05 +0300

    PressHouse solution update

--- Exam-2020-04-08/02.PressHouse/pressHouse.js
+++ Exam-2020-04-08/02.PressHouse/pressHouse.js
@@ -12,12 +12,9 @@ function solveClasses() {
     }
 
     class ShortReports extends Article {
-        constructor(title, content, originalResearches, comments) {
+        constructor(title, content, originalResearches) {
             super(title, content);
-            this.originalResearches = {
-                title,
-                author
-            };
+            this.originalResearches = originalResearches;
             this.comments = [];
         }
 
@@ -33,13 +30,70 @@ function solveClasses() {
         }
 
         get originalResearches() {
-            if (!(this.offscreenBuffering.hasOwnProperty(title) && this.offscreenBuffering.hasOwnProperty(author))) {
+            return this._originalResearches;
+        }
+        set originalResearches(value) {
+            if (!(value.hasOwnProperty('title') && value.hasOwnProperty('author'))) {
                 throw new Error("The original research should have author and title.");
             }
+
+            this._originalResearches = ({ ...this._originalResearches, ...value });
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+            return `The comment is added.`;
+        }
+
+        toString() {
+            let result = [super.toString()];
+
+            result.push(`Original Research: ${this.originalResearches.title} by ${this.originalResearches.author}`);
+            if(this.comments.length > 0) {
+                result.push('Comments:');
+                result.push(this.comments.join('\n'));
+            }
+
+            return result.join('\n')
         }
     }
 
-    class BookReview {
+    class BookReview extends Article{
+        constructor(title, content, book) {
+            super(title, content);
+            this.book = book;
+            this.clients = [];
+        }
+
+        addClient(clientName, orderDescription){
+            let client = this.clients.find(c => c.clientName === clientName);
+
+            if(!client) {
+                client = {
+                    clientName,
+                    orderDescription
+                }
+                this.clients.push(client);
+
+                return `${client.clientName } has ordered a review for ${this.book.name}`;
+            }
+
+            if(client.orderDescription === orderDescription) {
+                throw new Error(`This client has already ordered this review.`);
+            }
+        }
+
+        toString() {
+            let result = [super.toString()];
+            result.push(`Book: ${this.book.name}`);
+
+            if(this.clients.length > 0) {
+                result.push(`Orders:`);
+                result.push(`${this.clients.map(cl => `${cl.clientName} - ${cl.orderDescription}`).join('\n')}`);
+            }
+
+            return result.join('\n');
+        }
     }
 
     return {
@@ -49,12 +103,34 @@ function solveClasses() {
     }
 }
 
-let classes = solveClasses();
-// let lorem = new classes.Article("Lorem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non tortor finibus, facilisis mauris vel…");
-// console.log(lorem.toString());
-// // ------------------------------
-// let short = new classes.ShortReports("SpaceX and Javascript", "Yes, its damn true.SpaceX in its recent launch Dragon 2 Flight has used a technology based on Chromium and Javascript. What are your views on this ?", { title: "Dragon 2", author: "wikipedia.org" });
-// console.log(short.addComment("Thank god they didn't use java."));
-// short.addComment("In the end JavaScript's features are executed in C++ — the underlying language.");
-// console.log(short.toString()); 
-let short = new classes.ShortReports('Title', 'Content', {title: "Dragon 2", author: ''})
+let s = 'Yes, its damn true.Spac \
+eX in its recent launch Dragon 2 Flight has used a technology based on Chromium and Ja \
+vascript. What are your views on this ?';
+
+console.log(s.length);
+let classes = solveClasses();
+let lorem = new classes.Article('Lorem', 'Lorem ipsum dolor sit amet, consectetur adip \
+iscing elit. Fusce non tortor finibus, facilisis mauris vel…');
+console.log(lorem.toString()); 
+
+console.log('------------------------------');
+
+let short = new classes.ShortReports('SpaceX and Javascript', 'Yes, its damn true.Spac \
+eX in its recent launch Dragon 2 Flight has used a technology based on Chromium and Ja \
+vascript. What are your views on this ?', { title: 'Dragon 2', author: 'wikipedia.org'});
+
+console.log(short.addComment('Thank god they didn\'t use java.'));
+short.addComment('In the end JavaScript\'s features are executed in C++ — the underlying language.');
+console.log(short.toString()); 
+
+console.log('------------------------------');
+
+let book = new classes.BookReview('The Great Gatsby is so much more than a love story' 
+, 'The Great Gatsby is in many ways similar to Romeo and Juliet, yet I believe that it \
+ is so much more than just a love story. It is also a reflection on the hollowness of \
+a life of leisure. ...', { name: 'The Great Gatsby', author: 'F Scott Fitzgerald' });
+
+console.log(book.addClient('The Guardian', '100 symbols'));
+console.log(book.addClient('Goodreads', '30 symbols'));
+
+console.log(book.toString());
