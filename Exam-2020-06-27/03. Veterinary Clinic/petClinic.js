2f95af3453edf643e14a2a06577dc124e3a58168
Stanislav <stanislavtz@gmail.com>
2020-07-24 08:26:18 +0300

    Veterinary clinic upload

--- Exam-2020-06-27/03. Veterinary Clinic/petClinic.js
+++ Exam-2020-06-27/03. Veterinary Clinic/petClinic.js
@@ -17,35 +17,10 @@ class VeterinaryClinic {
         const pet = {
             petName,
             kind,
-            procedures            
+            procedures: []           
         }
 
-        Object.defineProperty(pet, 'totalProcedures', function() {return pet.procedures.length})
-
-        if (customer.pets.map(p => p.petName).indexOf(petName) === -1) { // to change it with find - additionally
-            customer.pets.push(pet);
-        }
-
-        let totalPetsInClinic = this.clients.reduce((acc, client) => {
-            const allCLientPets = client.pets.length;
-            acc += allCLientPets;
-            return acc;
-        }, 0);
-
-        if (this.capacity < totalPetsInClinic) {
-            throw new Error("Sorry, we are not able to accept more patients!");
-        }
-
-        const currentClient = this.clients.find(c => c.ownerName === ownerName);
-        if (currentClient) {
-            const currentPet = currentClient.pets.find(p => p.petName === petName);
-            console.log(currentPet)
-            // throw new Error(`This pet is already registered under ${ownerName} name! ${petName} is on our lists, waiting for ${currentPet.procedures.join(', ')}.`)
-        }
-        
-        this.clients.push(customer);
-        this.workflow.push(...pet.procedures);
-
+       
         return `Welcome ${petName}!`;
     }
 
