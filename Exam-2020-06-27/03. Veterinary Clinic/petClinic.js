0c70ca59e2ecbeb2dca9532a49112605cd3be329
Stanislav <stanislavtz@gmail.com>
2020-07-24 23:29:11 +0300

    Vet Clinic temporary update

--- Exam-2020-06-27/03. Veterinary Clinic/petClinic.js
+++ Exam-2020-06-27/03. Veterinary Clinic/petClinic.js
@@ -4,32 +4,85 @@ class VeterinaryClinic {
         this.clinicName = clinicName;
         this.capacity = capacity;
         this.clients = [];
-        this.workflow = [];
-        this.profit = 0;
+        this.currentWorkload = [];
+        this.totalProfit = 0;
     }
 
     newCustomer(ownerName, petName, kind, procedures) {
-        const customer = {
-            ownerName,
-            pets: []
+        let totalPetsInClinic = this.clients.reduce((acc, client) => {
+            acc += client.pets.length;
+            return acc;
+        }, 0);
+
+        if (totalPetsInClinic >= this.capacity) {
+            throw new Error("Sorry, we are not able to accept more patients!");
         }
 
-        const pet = {
-            petName,
-            kind,
-            procedures: []           
+        let customer = this.clients.find(c => c.ownerName === ownerName);
+        if (!customer) {
+            customer = {
+                ownerName,
+                pets: []
+            }
         }
 
-       
+        let pet = customer.pets.find(p => p.petName === petName);
+        if (!pet) {
+            pet = {
+                petName,
+                kind,
+                proc: procedures
+            }
+
+            customer.pets.push(pet);
+        } else {
+            const fullSetOfProcs = function () {
+                if (JSON.stringify(procedures.sort((a, b) => a.localeCompare(b))) === JSON.stringify(pet.proc.sort((a, b) => a.localeCompare(b)))) {
+                    return true;
+                }
+
+                for (const p of procedures) {
+                    if (pet.proc.includes(p)) {
+                        procedures = procedures.filter(pr => pr !== p);
+                    }
+                }
+
+                pet.proc.push(procedures);
+                return false;
+            }
+
+            if (fullSetOfProcs) {
+                throw new Error(`This pet is already registered under ${ownerName} name! ${petName} is on our lists, waiting for ${pet.proc.join(', ')}.`)
+            }
+        }
+
+
+        pet.proc.push(...procedures);
+        pet.proc = Array.from(new Set(pet.proc));
+        this.currentWorkload.push(...pet.proc);
+        this.clients.push(customer);
+
         return `Welcome ${petName}!`;
     }
 
+    onLeaving(ownerName, petName) {
+        let customer = this.clients.find(c => c.ownerName === ownerName);
+        if (!customer) {
+            throw new Error("Sorry, there is no such client!");
+        }
+
+        let pet = customer.pets.find(p => p.petName === petName);
+        if (!pet || pet.procedures.length === 0) {
+            throw new Error(`Sorry, there are no procedures for ${petName}!`)
+        }
+    }
 }
 
 let clinic = new VeterinaryClinic('SoftCare', 10);
 console.log(clinic.newCustomer('Jim Jones', 'Tom', 'Cat', ['A154B', '2C32B', '12CDB']));
+// console.log(clinic.newCustomer('Jim Jones', 'Tom', 'Cat', ['A154B']));
 console.log(clinic.newCustomer('Anna Morgan', 'Max', 'Dog', ['SK456', 'DFG45', 'KS456']));
-console.log(clinic.newCustomer('Jim Jones', 'Tiny', 'Cat', ['A154B'])); 
+console.log(clinic.newCustomer('Jim Jones', 'Tiny', 'Cat', ['A154B']));
 // console.log(clinic.onLeaving('Jim Jones', 'Tiny'));
 // console.log(clinic.toString());
 // clinic.newCustomer('Jim Jones', 'Sara', 'Dog', ['A154B']); 
